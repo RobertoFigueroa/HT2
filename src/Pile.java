a7e0ceb2c3c0ada5637fcfcd8896cfe04b01400e
Michael Steven Chan Gonzalez <cha18562@uvg.edu.gt>
2019-01-27 17:30:52 -0600

    Interfaz

--- src/Pile.java
+++ src/Pile.java
@@ -1,5 +1,33 @@
+import java.util.Vector;
 
-public class Pile implements Stack {
+public class Pile<T> implements Stack<T> {
 
+    private Vector<T> Data = new Vector<T>(2,0);
 
+    @Override
+    public void push(Object item) {
+
+    }
+
+    @Override
+    public T pop() {
+        T item = Data.lastElement();
+        Data.remove(-1);
+        return item;
+    }
+
+    @Override
+    public T peek() {
+        return Data.lastElement();
+    }
+
+    @Override
+    public boolean empty() {
+        return Data.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return Data.size();
+    }
 }
