5d103517439dab53809b3c6a11fa35f605f72ad4
RobertoFigueroa <35494933+RobertoFigueroa@users.noreply.github.com>
2019-02-02 21:25:22 -0600

    Archivos olvidados
    En este commit se agregaron los archivos de JavaDoc y el diagrama de secuencias que fueron olvidados de subir en Canvas.

--- src/MyCalculator.java
+++ src/MyCalculator.java
@@ -1,5 +1,18 @@
+/**
+ * Esta clase permite crear una calculadora para realizar operaciones aritemticas básicas.
+ * Esta clase implementa la iterfaz Calculadora
+ * @author Roberto Figueroa , Steven Chan
+ *
+ * */
 public class MyCalculator implements Calculator {
 
+    /**
+     * Calcula para dos numeros cualquier operación aritméitca simple definida por un signo de operación
+     * @param num1 Primer numero entero
+     * @param num2 Segundo numero entero
+     * @param op Signo para la operación
+     * @return Brinda la operación dada entro los dos numero y el signo aritémtico correspondiente
+     * */
     @Override
     public int calculate(int num1, int num2, String op){
         if(op.equals("+")){
